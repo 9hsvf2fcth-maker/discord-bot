7498498ee2e7b30c815156bd026ec73c382cf8a3
9hsvf2fcth-maker <9hsvf2fcth@privaterelay.appleid.com>
2026-02-22 15:45:53 +0000

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
+});
+
+client.once('ready', async () => {
+  console.log("Bot connecté");
+
+  const channel = await client.channels.fetch("ID_DU_SALON_VOCAL");
+
+  joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+});
+
+client.login(process.env.TOKEN);
